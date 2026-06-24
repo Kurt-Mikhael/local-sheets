40754556c9da0f5daec464990f0f5e8ea129e2b2
Kurt-Mikhael <kurtmikhael123@gmail.com>
2026-06-24 13:28:31 +0700

    Feat: migration

--- lib/client/composition.ts
+++
@@ -1,6 +0,0 @@
-import { SyncService } from '@/lib/application/sync-service'
-import { DexieWorkbookRepository } from '@/lib/infrastructure/local/dexie-workbook-repository'
-import { HttpSyncTransport } from '@/lib/infrastructure/remote/http-sync-transport'
-
-export const workbookRepository = new DexieWorkbookRepository()
-export const syncService = new SyncService(workbookRepository, new HttpSyncTransport())
