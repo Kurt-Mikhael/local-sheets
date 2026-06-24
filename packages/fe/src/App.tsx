aee4169798a297f25a1b321a0cdb929280c18510
Kurt-Mikhael <kurtmikhael123@gmail.com>
2026-06-24 13:32:09 +0700

    Feat: migration

---
+++ packages/fe/src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import OfflinePage from './pages/OfflinePage'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/offline" element={<OfflinePage />} />
+    </Routes>
+  )
+}
