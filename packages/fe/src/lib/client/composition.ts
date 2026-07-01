7a29fbdd63b3085e21ba40b8fcbb0668034d12a0
Kurt-Mikhael <kurtmikhael123@gmail.com>
2026-07-01 15:26:47 +0700

    Fixing: admin and user layout page

--- packages/fe/src/lib/client/composition.ts
+++ packages/fe/src/lib/client/composition.ts
@@ -1,6 +1,6 @@
-import { SyncService } from '@/lib/application/sync-service'
-import { DexieWorkbookRepository } from '@/lib/infrastructure/local/dexie-workbook-repository'
-import { HttpSyncTransport } from '@/lib/infrastructure/remote/http-sync-transport'
+import { SyncService } from '../application/sync-service'
+import { DexieWorkbookRepository } from '../infrastructure/local/dexie-workbook-repository'
+import { HttpSyncTransport } from '../infrastructure/remote/http-sync-transport'
 
 export const workbookRepository = new DexieWorkbookRepository()
 export const syncService = new SyncService(workbookRepository, new HttpSyncTransport())
